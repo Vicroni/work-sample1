f0c2f6e94f306a1c4c9df9633252c5076a866530
Saúl F. González Domínguez <Drasa_Tec@hotmail.com>
2024-10-29 17:23:59 -0600

    Initial commit

---
+++ servidor/lib/lambda/disconnect/index.mjs
@@ -0,0 +1,9 @@
+export const handler = async (event) => {
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Connect'),
+    };
+    return response;
+  };
+  
