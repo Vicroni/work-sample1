5d117aaa2d657a257a7330ec5f6acd956a28d4a1
Saúl F. González Domínguez <Drasa_Tec@hotmail.com>
2024-10-29 17:42:36 -0600

    Delete servidor directory

--- servidor/lib/lambda/disconnect/index.mjs
+++
@@ -1,9 +0,0 @@
-export const handler = async (event) => {
-    // TODO implement
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify('Connect'),
-    };
-    return response;
-  };
-  
